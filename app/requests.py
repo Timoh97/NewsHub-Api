7aca5ec5ccd6a4ea5d449020b8c12a35110b9c39
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-11-03 00:19:48 +0300

    deployment to heroku

--- app/requests.py
+++ app/requests.py
@@ -2,7 +2,7 @@ from newsapi import NewsApiClient
 # from ..config import from news
  
 class NewsRequests:
-   API_KEY = ''
+   API_KEY = '2c668aaf75c744c190238ad94b7efe83'
    n = NewsApiClient(api_key=API_KEY)
   
    def get_top_headlines(self,**kwargs):
